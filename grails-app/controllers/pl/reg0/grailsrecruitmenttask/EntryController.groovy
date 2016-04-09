0aa94a9c1b579e77eed2d9ccb960957210262840
Kamil Mikolajczyk <rego@gazeta.pl>
2016-04-09 04:17:32 +0200

    added tasks

--- grails-app/controllers/pl/reg0/grailsrecruitmenttask/EntryController.groovy
+++ grails-app/controllers/pl/reg0/grailsrecruitmenttask/EntryController.groovy
@@ -5,7 +5,15 @@ class EntryController {
     EntryService entryService
 
     def index(String filter) {
-        [entries: filter ? entryService.filter(filter) : entryService.list()]
+        if (filter) {
+            String subject = message(code: 'email.subject')
+            Closure contentClosure = { String authorName ->
+                message(code: 'email.content', args: [authorName])
+            }
+            [entries: entryService.filter(filter, subject, contentClosure)]
+        } else {
+            [entries: entryService.list()]
+        }
     }
 
 }
--- grails-app/services/pl/reg0/grailsrecruitmenttask/EntryService.groovy
+++ grails-app/services/pl/reg0/grailsrecruitmenttask/EntryService.groovy
@@ -8,10 +8,27 @@ class EntryService {
         BlogEntry.list([sort: 'id', order: 'desc'])
     }
 
-    List<BlogEntry> filter(String filter) {
-        // FIXME: (1) fix database query performace
-        BlogEntry.list().findAll {
+    List<BlogEntry> filter(String filter, String subject, Closure contentClosure) {
+        // FIXME: (1) evaluate and fix database query performace
+        List entries = BlogEntry.list().findAll {
             it.contents.contains(filter) || it.title.contains(filter) || it.author.name.contains(filter)
         }
+
+        // TODO: (2) how to avoid duplicated authors here using simplest code?
+        notifyAuthors(entries*.author, subject, contentClosure)
+
+        return entries
+    }
+
+    private void notifyAuthors(List<Author> authors, String subject, Closure contentClosure) {
+        authors.each {
+            // TODO: (3) what are advantages of using closure here? how it works? why couldn't or shouldn't we call message(code: '...') here in service?
+            sendNotfication(subject, contentClosure(it.name))
+        }
+    }
+
+    private void sendNotfication(String subject, String contents) {
+        // simulating external service which sends emails
+        System.out.println("Sending email with subject: $subject.\nContents: $contents")
     }
 }
